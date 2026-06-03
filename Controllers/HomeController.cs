059e9066101fde929e4f013f32a56b0d428b7846
TEO BENJAMIN MORENO <50291929@ort>
2026-06-03 08:35:28 -0300

    Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,8 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        PalabrasAhorcado palabrasAhorcado = new PalabrasAhorcado();
+        Viewbag.palabrasAhorcado = palabrasAhorcado;
         return View();
     }
 
