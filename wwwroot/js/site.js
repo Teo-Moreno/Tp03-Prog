ae434ea7181d8c178eb11aebf1ef78d375e5d799
TEO BENJAMIN MORENO <50291929@ort>
2026-06-03 09:01:34 -0300

    javascript creado

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿ArriesgarLetra();
+{
+  const Letra = getElementById("letra");
+  const Palabra = getElementById("palabra");
+  for (let i = 0; i < Palabra.Lenght; i++) {
+    if (Palabra[i] == Letra) {
+    }
+  }
+}
